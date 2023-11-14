e558659b12d0eec0fae73f49e94a0e45cc5267b3
alleycaaat <achulslander@gmail.com>
2023-11-14 12:54:57 -0600

    add loading screen

---
+++ src/loading/index.jsx
@@ -0,0 +1,10 @@
+import './loading.scss';
+
+export default function Loading() {
+    return (
+        <div id='wrapper'>
+            <div id='loading-text'>LOADING</div>
+            <div id='loading-content'></div>
+        </div>
+    );
+}
