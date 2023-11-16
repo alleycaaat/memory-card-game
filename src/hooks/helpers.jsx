77f78b85ceb02a46958952c2b51fe820028d253c
alleycaaat <achulslander@gmail.com>
2023-11-16 16:53:45 -0600

    fixed the database ID`s

--- src/hooks/helpers.jsx
+++ src/hooks/helpers.jsx
@@ -1,19 +1,21 @@
 import { databases } from './api';
 
-const DATABASE_ID = '653c07f36886d745e028';
-
 //** get the cards from the right deck
 export const getCards = async (category) => {
     let collectionId;
+    let DATABASE_ID;
     switch (true) {
         case category == 'programming':
+            DATABASE_ID = '653c07f36886d745e028';
             collectionId = '653c0af237cb305d0e3f';
             break;
         case category == 'colors':
-            collectionId = '6542a9463c9ed33053d4';
+            DATABASE_ID = '6542a9463c9ed33053d4';
+            collectionId = '6542a95f05a2299376d6';
             break;
         case category == 'math':
-            collectionId = '6542ac00dbf00f97c906';
+            DATABASE_ID = '6542ac00dbf00f97c906';
+            collectionId = '6542ac05a9938ee0abe8';
             break;
         default:
             break;
