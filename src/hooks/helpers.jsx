a28f01b43eaaf6a362ce434d63caa591b43727d4
alleycaaat <achulslander@gmail.com>
2024-01-19 13:08:49 -0600

    update collect IDs

--- src/hooks/helpers.jsx
+++ src/hooks/helpers.jsx
@@ -4,24 +4,25 @@ import { databases } from './api';
 export const getCards = async (category) => {
     let collectionId;
     let DATABASE_ID;
+    const DB = '653c07f36886d745e028'
 
     switch (true) {
         case category == 'programming':
-            DATABASE_ID = '653c07f36886d745e028';
+            DATABASE_ID = DB;
             collectionId = '653c0af237cb305d0e3f';
             break;
         case category == 'colors':
-            DATABASE_ID = '6542a9463c9ed33053d4';
-            collectionId = '6542a95f05a2299376d6';
+            DATABASE_ID = DB;
+            collectionId = '65aac5acd011dc02c7b5';
             break;
         case category == 'math':
-            DATABASE_ID = '6542ac00dbf00f97c906';
-            collectionId = '6542ac05a9938ee0abe8';
+            DATABASE_ID = DB;
+            collectionId = '65aac74e44bc9b59f23b';
             break;
         default:
             break;
     }
-    
+
     try {
         var docs = await databases.listDocuments(DATABASE_ID, collectionId);
         return docs.documents;
